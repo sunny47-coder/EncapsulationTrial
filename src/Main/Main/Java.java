3288a1780d81bdbf50161c6dca4910f7ba284bca
sunny47-coder <marvinodia86@gmail.com>
2026-06-19 01:52:12 +0800

    Changing car color to blue

--- src/Main/Main/Java.java
+++ src/Main/Main/Java.java
@@ -2,7 +2,7 @@ package Main.Main;
 
 public class Java {
 public static void main(String[] args) {
-    Car car = new Car("Toyota", "Red");
+    Car car = new Car("Toyota", "Blue");
     System.out.println(car.getName());
 }
 }
